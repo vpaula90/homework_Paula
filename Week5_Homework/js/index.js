c43855c4d73a5db9a76aaced9375305ce0200fe7
Paula <vpaula90@gmail.com>
2018-01-17 19:14:19 -0800

    Week5_Homework

---
+++ Week5_Homework/js/index.js
@@ -0,0 +1,11 @@
+
+var showImage = 'on';
+var hideImage = null;
+
+
+$('#yourButton').on('click', yourFunction);
+
+function yourFunction()
+{
+    $('#result').attr('src','./images/download (1).jpeg');
+}
