f971b94218a9a0aaad99ab1d312b4d1203085c2a
Paula <vpaula90@gmail.com>
2018-04-14 10:24:05 -0700

    Week1_JSHomeWorkd

---
+++ jsweek1_Homework/script.js
@@ -0,0 +1,39 @@
+/*Define 5 variables
+Assign each data type to 1 of the five variables (string, number, boolean, object, function)
+Access each variable in some way
+
+Bonus: Use 3 different assignment operators
+Bonus: Use 2 different logic operators
+Bonus: Use and if/else  */
+
+var name = "Khal Drogo";
+console.log(name);
+
+var kitten = false;
+console.log(kitten);
+
+var age = 4;
+console.log(age);
+
+var color = {
+  'eyes':'green',
+  'coat':'grey-white'
+};
+console.log(color.eyes);
+
+var lovesFood = function()
+{
+  console.log("Lotus Wet Food");
+}
+lovesFood();
+
+var color = {
+  'eyes': ['green', 'blue'],
+  'coat': ['grey-white', 'black']
+};
+console.log(color.eyes[0]);
+
+
+  var food = "Evo";
+
+console.log("Khal Loves " + food);
